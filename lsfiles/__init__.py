fbfce6be1dc35deb2ab66ceb8175079bf361adcf
Aymeric Guth <aymeric.guth@protonmail.com>
2022-07-01 18:00:26 +0200

    fixed imports at package level

--- lsfiles/__init__.py
+++ lsfiles/__init__.py
@@ -1,6 +1,6 @@
 from ._lsfiles import iterativeBFS, iterativeDFS, recursiveDFS, is_leaf
 from . import filters
 from . import adapters
-from ._types import Maybe, LSFilesError
+from ._types import Maybe, LSFilesError, InPath, PathGeneric
 
 __version__ = '0.0.2'
